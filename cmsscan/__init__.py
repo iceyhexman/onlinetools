303f3806efd9cc8e8f9b1b02b007bc922c5fb4fc
iceyhexman <834430486@qq.com>
2018-02-02 15:20:35 +0800

    add c段

--- cmsscan/__init__.py
+++ cmsscan/__init__.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, make_response
 from .model.whatcms import gwhatweb
 import re
+import requests
+import json
+import socket
+
 app = Flask(__name__)
+
+
+def getjson():
+    return json.loads(request.get_data())
 
 
 @app.route('/')
@@ -17,11 +25,11 @@ def whatcms():
         url = request.form.get("url")
         if re.match(r'^https?:/{2}\w.+$', url):
             try:
-                whatcmsResult = gwhatweb(url).whatweb()
-                return render_template('whatcms.html', data=whatcmsResult, title='CMS识别')
+                whatcmsresult = gwhatweb(url).whatweb()
+                return render_template('whatcms.html', data=whatcmsresult, title='CMS识别')
             except:
-                whatcmsResult = {'total': '', 'url': '', 'result': '', 'time': ''}
-                return render_template('whatcms.html', data=whatcmsResult, title='CMS识别')
+                whatcmsresult = {'total': '', 'url': '', 'result': '', 'time': ''}
+                return render_template('whatcms.html', data=whatcmsresult, title='CMS识别')
     else:
         return render_template('whatcms.html', title="CMS识别")
 
@@ -34,5 +42,39 @@ def jsfuck():
 @app.route('/getdomain')
 def getdomin():
     return render_template('getdomain.html', title='getdomain')
+
+
+#查询
+@app.route('/api/query', methods=['post'])
+def query_c():
+    post_json = getjson()
+    request_json_raw = requests.get('http://www.webscan.cc/?action=query&ip=%s' % post_json[0]['ip'])
+    return request_json_raw.content
+
+
+# 文件下载
+@app.route('/api/download', methods=['POST'])
+def download_file():
+    content = request.get_data()
+    response = make_response(content)
+    response.headers['Content-Disposition'] = 'attachment; filename=data.txt'
+    return response
+
+
+# domain2ip
+@app.route('/api/domain2ip', methods=['POST'])
+def return_json():
+    domain_json = getjson()
+    ip = socket.gethostbyname(domain_json[0]['domain'].split('/')[2])
+    j_ip = [{"ip": ip}]
+    return jsonify(j_ip)
+
+
+# thread_start
+@app.route('/api/thread',methods=['post'])
+def thread_start():
+    thread_ip = getjson()
+    thread_json_raw = requests.get('http://webscan.cc/thread.php?ip=%s' % thread_ip[0]['ip'])
+    return thread_json_raw.content
 
 
